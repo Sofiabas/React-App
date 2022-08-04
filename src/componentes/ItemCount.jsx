b88b7196563a4e7394ec068a00ef10fbe6a37d01
Sofia Basile <sofibasile86@gmail.com>
2022-08-04 11:41:54 -0300

    desafio3

---
+++ src/componentes/ItemCount.jsx
@@ -0,0 +1,27 @@
+import React,{useState} from "react";
+
+const ClickCounter = ({stock,initial,onAdd} ) =>{
+    const [click,setClick] = useState(initial);
+
+   const incremento= ()=> {
+    setClick(click+1);
+   }
+
+   const decremento= ()=> {
+    setClick(click-1);
+   }
+       return(
+        <>
+        <h1>CONTADOR DE CLICK</h1>
+        <h2> Clicks:{click}</h2>
+        <button disabled={click >= stock} onClick={incremento} class="btn btn-primary">+</button>
+        <button disabled={click <=1 } onClick={decremento} class="btn btn-primary">-</button>
+        <button onClick={()=> setClick(1)} class="btn btn-danger">Reseteo</button>
+        <div>
+        <button  disabled={stock <= 0 } onClick={()=> onAdd(click)} class="btn btn-success">Añadido al Carrito</button>
+        </div>
+        </>
+    )
+}
+
+export default ClickCounter;
--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import ItemCount from '../componentes/ItemCount';
 import './ItemListContainer.css';
 
+
 function ItemListContainer(props) {
+
+const onAdd=(quantity)=>{
+console.log(`Añadio ${quantity} unidades`);
+}
   return (
+    <>
     <h1 className='titulo'>{props.greeting} Titulo de Prueba </h1>
+    <div><ItemCount stock={30} initial={1} onAdd={onAdd} /> 
+    </div>
+    </>
   )
 }
-
 export default ItemListContainer
